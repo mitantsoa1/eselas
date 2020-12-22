26db83c03a090d05f65fb63ac23b4f1b5247d145
mitantsoa1 <ainamitantsogit@gmail.com>
2020-12-22 09:17:15 +0300

    Decommenter modal-footer

--- .history/views/malagasy_20201222091534.php
+++ .history/views/malagasy_20201222091534.php
@@ -236,7 +236,7 @@
         </div>
         
         Modal footer -->
-        <div class="modal-footer"> 
+        <!-- <div class="modal-footer">
        
           <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
         </div>
